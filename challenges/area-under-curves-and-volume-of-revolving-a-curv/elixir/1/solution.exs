61a00d142b744b0c6dd07988d428d06b212463c4
Ioannis (John) Bourlakos <jbourlakos@users.noreply.github.com>
2022-03-16 03:40:09 +0200

    challenges/area-under-curves-and-volume-of-revolving-a-curv: Elixir
    WIP

---
+++ challenges/area-under-curves-and-volume-of-revolving-a-curv/elixir/1/solution.exs
@@ -0,0 +1,51 @@
+# Try it at: https://www.hackerrank.com/challenges/area-under-curves-and-volume-of-revolving-a-curv/problem
+
+defmodule Solution do
+
+  @subintervals_length 0.001
+  
+  def ints_from_line do
+    IO.read(:line)
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+  end
+  
+  def reduce_polynomial(polynomial) do
+    polynomial
+    |> Enum.sort(&<=/2)
+    |> _reduce_polynomial([])
+    |> Enum.reverse
+  end
+  defp _reduce_polynomial([], reduced), do: reduced
+  defp _reduce_polynomial( [{e,c}], reduced ), do: [ {e,c} | reduced ]
+  defp _reduce_polynomial( [ {e,c1}, {e,c2} | rest ], reduced ) do
+    _reduce_polynomial(rest, [{e,c1+c2}|reduced])
+  end
+  defp _reduce_polynomial( [ {e1,c1}, {e2,c2} | rest ], reduced ) do
+    _reduce_polynomial( rest, [ {e1,c1}, {e2,c2} | reduced ] )
+  end
+  
+  def to_polynomial(exponents, coefficients) do
+    List.zip([exponents, coefficients])
+    |> reduce_polynomial()
+  end
+  
+  # TODO: add more functions here
+  
+  def main do
+
+    coefficients = ints_from_line()
+    exponents = ints_from_line()
+    [left, right] = ints_from_line()
+    
+    polynomial = to_polynomial(exponents, coefficients)
+    
+    IO.inspect(polynomial)
+    
+    # TODO: finish main
+
+  end
+  
+end
+
+Solution.main
